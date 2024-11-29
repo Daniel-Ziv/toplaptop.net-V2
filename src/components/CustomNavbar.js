fae4bcc2445206dee4ee072203cb768874c51969
daniel-ziv <selerbi3@gmail.com>
2024-11-29 16:30:38 +0200

    nothing same thing

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -20,14 +20,14 @@ function CustomNavbar({ currentStep, prevStep, nextStep }){
 
   return (
     <Navbar position={currentStep != 1 ? 'sticky' : 'static'} className="h-12">
-    <NavbarContent justify="start">
+    <NavbarContent justify="start" >
      <NavbarItem>
      {currentStep != 1 ? (
       <Button 
         onClick={handlePrevStep}
         color="default"
         variant="flat"
-        className="flex items-center gap-0 text-black mt-1 bg-gray-200"
+        className="flex items-center gap-0 text-black mt-1 bg-gray-200 "
       >
         <ArrowRight strokeWidth={1.3} />
         חזור
@@ -47,7 +47,7 @@ function CustomNavbar({ currentStep, prevStep, nextStep }){
      </NavbarItem>
     </NavbarContent>
     {!showNext ? (
-       <NavbarBrand className="justify-end">
+       <NavbarBrand className="justify-end ">
          <a href="/" className="flex items-center">
            <p className="font-bold text-inherit">
              <mark className="px-2 text-white bg-black rounded dark:bg-black mr-0.5">top</mark> laptop
